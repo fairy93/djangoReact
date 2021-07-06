3d4c3a0f1d6c019ceff848b0b72c60c95fe0e256
fairy93 <kimdongju3331@gmail.com>
2021-07-06 11:03:24 +0900

    settings.py 보안변경

---
+++ backend/mini/my_setting.py
@@ -0,0 +1,12 @@
+SECRET = 'django-insecure-690%i12d*wmuhu_#bq528u9rns7a*q=e=q1801)yy(4k7afrgf'
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
